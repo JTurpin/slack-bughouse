3ed15b9cd2bba6b625a904cc3df56a07330260cb
Jim Turpin <jim.turpin@logrhythm.com>
2018-08-21 13:12:55 -0600

    added helpm chars and color output to test-it

--- tests/test-it.sh
+++ tests/test-it.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-curl --connect-timeout 2 -v http://slack-bughouse:9090/teams?text=player1%20player2%20player3%20player4
-#curl --connect-timeout 2 -v http://slack-bughouse:9091/teams?text=player1%20player2%20player3%20player4
+RED='\033[0;31m'
+GREEN='\033[1;32m'
+NC='\033[0m' # No Color
+
+#curl -s --connect-timeout 2 -v http://slack-bughouse:9090/teams?text=player1%20player2%20player3%20player4
+curl -s --connect-timeout 2 -v http://slack-bughouse:9091/teams?text=player1%20player2%20player3%20player4
+
+if [ $? -eq 0 ]
+then
+  echo -e "${GREEN}All tests passed!${NC}"
+else
+  echo -e "${RED}Tests FAILED${NC}" >&2
+fi
